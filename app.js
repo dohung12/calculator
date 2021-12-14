638f05f02dcd8ce2599969641faa8acbd56fc84b
dohung <dohuyhung12@gmail.com>
2021-12-14 10:34:23 +0700

    add operate fn

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// VARIABLES
+
 // FUNCTION
 function plus(num1, num2) {
   return num1 + num2;
@@ -13,4 +15,8 @@ function times(num1, num2) {
 
 function divide(num1, num2) {
   return num1 / num2;
+}
+
+function operate(operator, num1, num2) {
+  return operator(num1, num2);
 }
