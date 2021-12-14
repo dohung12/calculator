3866e4daf360f535b112aac6f8e13a398a85f915
dohung <dohuyhung12@gmail.com>
2021-12-14 10:28:57 +0700

    add 4 basic function

---
+++ app.js
@@ -0,0 +1,16 @@
+// FUNCTION
+function plus(num1, num2) {
+  return num1 + num2;
+}
+
+function minus(num1, num2) {
+  return num1 - num2;
+}
+
+function times(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
